ad1637a7936b6ad894fe4fff4c776a1a3ebf774d
Atharva Kumar <atharvados12121@gmail.com>
2024-10-19 23:18:50 +0530

    Did 3 questions in Div2. Not satisfied as i seen some hints for C. Let's see for the next one.

---
+++ CF_Contests/Codeforces Round 979 (Div. 2)/B_Minimise_Oneness.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define POP pop_back
+#define MOD 1e7 + 10
+#define PI 3.1415926535897932
+#define PB push_back
+#define MP make_pair
+#define pb push_back
+#define ppb pop_back
+int power(int a, int b)
+{
+    int r = 1;
+    while (b)
+    {
+        if (b & 1)
+            r = (r * a);
+        a = (a * a);
+        b /= 2;
+    }
+    return r;
+}
+
+typedef long long int ll;
+typedef pair<ll, ll> pll;
+typedef map<ll, ll> mll;
+typedef vector<ll> Vi;
+#define IOS                  \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define fori(e, x) for (ll i = e; i < x; i++)
+#define forj(x) for (ll j = 0; j < x; j++)
+#define sorta(x) sort(x.begin(), x.end())
+#define fora(x) for (auto it : x)
+#define int long long
+int32_t main()
+{
+    IOS;
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        string s = "";
+        if(n==1){
+            cout<<"0"<<endl;
+            continue;
+        }
+        for (int i = 0; i <= ((n / 2) - 1); i++)
+        {
+            s.push_back('0');
+        }
+        s.push_back('1');
+        for (int i = (n / 2) + 1; i < n; i++)
+        {
+            s.push_back('0');
+        }
+        cout << s << endl;
+    }
+    return 0;
+}
